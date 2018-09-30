cad2e397720d5ef26154854105d37775c6eb5ae1
nachumfreedman <nochum09@gmail.com>
2018-09-30 03:10:36 +0300

    something git did

--- src/About.js
+++ src/About.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import './About.css';
 
-=======
-//import './About.css';
->>>>>>> b037d1df493b786fe31cae767535e96d72a1c287
 const Roaster = require("./imgs/roast.jpg");
 
 class About extends Component {
